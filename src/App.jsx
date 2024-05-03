936940a795f1146f4455e9f8dc6f20c9fec3c2db
Oleksandr <gayrsuk@gmail.com>
2024-05-03 21:05:05 +0300

    Solution
    Added Profile, Friends tit and transaction history

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css';
 import userData from './datas/userData.json';
 import friends from './datas/friends.json';
+import transactions from './datas/transactions.json';
+
 import Profile from './components/profile/Profile';
 import FriendList from './components/friendsList/FriendList';
-
+import TransactionHistory from './components/transactionHistory/TransactionHistory';
 
 const App = () => {
   return (
     <>
       <Profile {...userData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
---
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import './TransactionHistory.module.css';
+import TransactionHistoryItem from '../transactionHistoryItem/TransactionHistoryItem';
+import { Fragment } from 'react';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <Fragment key={id}>
+            <TransactionHistoryItem
+              type={type}
+              amount={amount}
+              currency={currency}
+            />
+          </Fragment>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
+
+TransactionHistory.propTypes = {
+  items: PropTypes.array.isRequired,
+};
