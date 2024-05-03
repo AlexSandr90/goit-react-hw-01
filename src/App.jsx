165be4dea602bae6105fd2b170939cc3c6aa72da
Oleksandr <gayrsuk@gmail.com>
2024-05-04 01:20:51 +0300

    refactored file paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import './App.css';
-import userData from './datas/userData.json';
-import friends from './datas/friends.json';
-import transactions from './datas/transactions.json';
-
-import Profile from './components/profile/Profile';
-import FriendList from './components/friendsList/FriendList';
-import TransactionHistory from './components/transactionHistory/TransactionHistory';
+import { userData, friends, transactions } from './datas';
+import { Profile, FriendList, TransactionHistory } from './components';
 
 const App = () => {
   return (
